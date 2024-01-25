aab53437e285b643ae892f0923f772ca1d5252f1
rickmujica <ricardo.urbieta@gmail.com>
2024-01-25 14:00:16 -0300

    fix another typo on index route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,7 +4,7 @@ export default function Index() {
     <main className="bg-white shadow">
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <h1 className="text-2xl md:text-3xl font-bold tracking-tight text-gray-900 mb-4">
-        This is React app was created to showcase the hability to consume Nasa's API
+        This React app was created to showcase the ability to consume NASA's API.
         </h1>
         <p>There are two links in the NavBar. The first one uses the Astronomy Picture of the Day endpoint, and the second link uses the NASA Tech Transfer Patents endpoint.</p>
       </div>
